8b58a1c7db5c2235cfb030c46d25991b7ac69843
AliEmre33 <aliemrebulbul@gmail.com>
2025-03-05 17:07:30 +0300

    Update D2_WebDriver.java
    düzenleme yapildi

--- src/test/java/Ders1_Exe/D2_WebDriver.java
+++ src/test/java/Ders1_Exe/D2_WebDriver.java
@@ -1,4 +1,29 @@
 package Ders1_Exe;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 public class D2_WebDriver {
+
+    @Test
+    public void testLogin() {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement usernameInput = driver.findElement(By.id("user-name"));
+        usernameInput.sendKeys("standard_user");
+
+        WebElement passwordInput = driver.findElement(By.id("password"));
+        passwordInput.sendKeys("secret_sauce");
+
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+
+        driver.quit();
+    }
 }
